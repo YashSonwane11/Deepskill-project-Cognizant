2a0d4708f8602b0a8a61d06b9b0557df119a6a60
MR.YashuHacker <100185004+Yashjihacker@users.noreply.github.com>
2026-07-12 23:33:28 +0530

    created new eureka and api gateway services in microservices

--- Microservices/api-gateway/src/main/java/com/cognizant/api_gateway/ApiGatewayApplication.java
+++ Microservices/api-gateway/src/main/java/com/cognizant/api_gateway/ApiGatewayApplication.java
@@ -1,13 +1,13 @@
-package com.cognizant.account_service;
+package com.cognizant.api_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AccountServiceApplication {
+public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AccountServiceApplication.class, args);
+		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
 }
