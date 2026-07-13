a693c4c0a2d297c3bc57113b76b7a7ff45070d1d
MR.YashuHacker <100185004+Yashjihacker@users.noreply.github.com>
2026-07-13 23:31:43 +0530

    Completed Microservices with Spring Boot, Eureka Discovery Server and API Gateway assignment

--- Microservices/account-service/src/main/java/com/cognizant/account_service/controller/AccountController.java
+++ Microservices/account-service/src/main/java/com/cognizant/account_service/controller/AccountController.java
@@ -1,20 +1,29 @@
 package com.cognizant.account_service.controller;
 
-import com.cognizant.account_service.model.Account;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 public class AccountController {
 
-    @GetMapping("/accounts/{number}")
-    public Account getAccount(@PathVariable String number) {
+    @Value("${server.port}")
+    private String port;
 
-        return new Account(
-                number,
-                "Savings",
-                50000
-        );
+    @GetMapping("/accounts/{number}")
+    public Map<String, Object> getAccount(@PathVariable String number) {
+
+        Map<String, Object> response = new HashMap<>();
+
+        response.put("accountNumber", number);
+        response.put("type", "Savings");
+        response.put("balance", 50000);
+        response.put("servedByPort", port);
+
+        return response;
     }
 }
